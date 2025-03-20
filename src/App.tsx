ac410332c570ab13a8e11f38a2a3002fe8efd209
nigussolomon <nigussolomon444@gmail.com>
2025-03-20 13:43:44 +0300

    implemented basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Root from "./pages/root";
+import Auth from "./pages/auth";
 
 export default function App() {
   return (
-    <div>
-      <Button>HALLO</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import "@mantine/notifications/styles.css";
 import "@mantine/dropzone/styles.css";
 import "@mantine/core/styles.css";
 
+
 const base: MantineColorsTuple = [
   "#fbefef",
   "#efdcdc",
